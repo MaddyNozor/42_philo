5a3b9f7d5fc802ff119cdbb1da952f2392357863
Mailys Riviere <mairivie@paul-f5Br6s6.clusters.42paris.fr>
2024-11-19 18:53:29 +0100

    19/11 debog parsing en cours

--- source/parsing.c
+++ source/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 15:36:02 by mairivie          #+#    #+#             */
-/*   Updated: 2024/11/19 17:56:21 by mairivie         ###   ########.fr       */
+/*   Updated: 2024/11/19 18:52:20 by mairivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,10 +56,10 @@ bool	looks_like_a_number(char *str)
 	int	i;
 
 	i = 0;
-	while (str[i] == ' ' || str[i] == '\t')
-		i++;
-	// if ((str[i] == '+' || str[i] == '-') && (str[i + 1]))
+	// while (str[i] == ' ' || str[i] == '\t')
 	// 	i++;
+	if ((str[i] == '+') && (str[i + 1]))
+		i++;
 	while (str[i])
 	{
 		if (!(ft_isdigit(str[i])))
@@ -72,7 +72,7 @@ bool	looks_like_a_number(char *str)
 bool	is_valid_int_number(char *str)
 {
 	if (str == NULL || *str == '\0')
-		return (ft_printf("t'es vide\n"), false); //TODO 000 en premier arg rentre dans cette boucle. why ???
+		return (ft_printf("%s t'es vide\n", str), false);
 	if (looks_like_a_number(str) == false)
 		return (ft_printf("looks not like an int\n"), false);
 	if (detect_int_overflow(str) == true)
@@ -88,24 +88,27 @@ int	check_parsing(int ac, char **av)
 	static char	*time_to_sleep;
 	static char	*nb_time_must_eat;
 	
-	if (ac < 6 || ac > 7)
+	ft_printf("ac = %i\n", ac);
+	if (ac < 5 || ac > 6)
 		return (ft_printf("nb arg error\n"), FAILURE);
-	nb_philo = av[2];
-	time_to_die = av[3];
-	time_to_eat = av[4];
-	time_to_sleep = av[5];
-	if (ac == 6)
-		nb_time_must_eat = av[6];
+ft_printf("arg 1:%s 2:%s 3:%s 4:%s 5:%s 6:%s \n", av[1], av[2], av[3], av[4], av[5], av[6], av[7]);
+	nb_philo = av[1];
+	time_to_die = av[2];
+	time_to_eat = av[3];
+	time_to_sleep = av[4];
+	if (ac == 6) //TODO y a ce cas a gerer. tsss clean code a faire
+		nb_time_must_eat = av[5];
 	if (is_valid_int_number(nb_philo) == false)
-		return(FAILURE);
+		return(ft_printf("philo t'es vide\n"), FAILURE);
 	if (is_valid_int_number(time_to_die) == false)
-		return(FAILURE);
+		return(ft_printf("die t'es vide\n"), FAILURE);
 	if (is_valid_int_number(time_to_eat) == false)
-		return(FAILURE);
+		return(ft_printf("eat t'es vide\n"), FAILURE);
 	if (is_valid_int_number(time_to_sleep) == false)
-		return(FAILURE);
+		return(ft_printf("sleep t'es vide\n"), FAILURE);
 	if (is_valid_int_number(nb_time_must_eat) == false)
-		return(FAILURE);
+		return(ft_printf("must t'es vide\n"), FAILURE);
 	return (SUCCESS);
 }
 
+		// ft_printf("MUST ?? %s \n", nb_time_must_eat);
